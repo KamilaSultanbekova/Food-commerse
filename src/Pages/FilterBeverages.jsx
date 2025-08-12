4fa6f76babe4d521edff8afb75e680c95cf96db6
KamilaSultanbekova <kaamiillkss@gmail.com>
2025-08-12 18:15:15 +0500

    all pages are created

---
+++ src/Pages/FilterBeverages.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FilterBeverages() {
+  return (
+    <div>FilterBeverages</div>
+  )
+}
---
+++ src/Pages/UpdateAcc.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UpdateAcc() {
+  return (
+    <div>UpdateAcc</div>
+  )
+}
